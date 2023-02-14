0b4f7e46e64e9de4fe17189d55ce72fd4d801194
adam-sagar <adamschrissagar@gmail.com>
2023-02-14 14:52:10 +1300

    npm and node examples

--- Module 1/Exercise 2/helloworld.js
+++ Module 1/Exercise 2/helloworld.js
@@ -1,1 +1,31 @@
 console.log('hello world')
+
+function sayHello() {
+    console.log('hello again')
+}
+
+sayHello()
+
+function sum(a, b) {
+    let result = a+b;
+    return result;
+}
+
+
+console.log(sum(88,44))
+
+sayHello()
+
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var result = sentiment.analyze('Cats are great.');
+console.dir(result);    // Score: -2, Comparative: -0.666
+
+function analyseSentence(sentence) {
+    var sentiment = new Sentiment();
+    var result = sentiment.analyze(sentence);
+
+    return result.scroe;
+}
+
+console.log(analyseSentence('I love chocolate but i hate that it makes me fat.'))
