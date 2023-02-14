86f4383dc10debcda9f1362d9532c7eeda741d5b
adam-sagar <adamschrissagar@gmail.com>
2023-02-14 19:01:48 +1300

    Day 2 exercises

--- Module 1/Exercise 2/helloworld.js
+++ Module 1/Exercise 2/helloworld.js
@@ -16,16 +16,25 @@ console.log(sum(88,44))
 
 sayHello()
 
-var Sentiment = require('sentiment');
-var sentiment = new Sentiment();
-var result = sentiment.analyze('Cats are great.');
-console.dir(result);    // Score: -2, Comparative: -0.666
+// var Sentiment = require('sentiment');
+// var sentiment = new Sentiment();
+// var result = sentiment.analyze('Cats are great.');
+// console.dir(result);    // Score: -2, Comparative: -0.666
 
-function analyseSentence(sentence) {
-    var sentiment = new Sentiment();
-    var result = sentiment.analyse(sentence);
+// function analyseSentence(sentence) {
+//     var sentiment = new Sentiment();
+//     var result = sentiment.analyse(sentence);
 
-    return result.score;
+//     return result.score;
+// }
+
+// console.log(analyseSentence('I love chocolate but i hate that it makes me fat.'))
+
+
+function sum(a, b) {
+    let result= a-b;
+    return result;
 }
 
-console.log(analyseSentence('I love chocolate but i hate that it makes me fat.'))
+console.log(sum(10,5))
+ 
