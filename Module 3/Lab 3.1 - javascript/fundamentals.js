111b3ae708e0437a67175bc79fb35a1ad0c9b27d
adam-sagar <adamschrissagar@gmail.com>
2023-02-21 19:04:42 +1300

    Lab 3.1 - javascript

---
+++ Module 3/Lab 3.1 - javascript/fundamentals.js
@@ -0,0 +1,15 @@
+console.log(Number("" + 1 + 0))
+console.log(Number("" - 1 + 0))
+console.log(Number(true + false))
+console.log(Number(!true))
+console.log(Number(6 / "3"))
+console.log(Number("2" * "3"))
+console.log(Number(4 + 5 + "px"))
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+
+let addition = three + four
+
+console.log(addition)
---
+++ Module 3/Lab 3.1 - javascript/practice.js
@@ -0,0 +1,45 @@
+let fruit = "apple";
+console.log(fruit);
+
+fruit = "orange";
+console.log(fruit);
+
+fruit = "banana";
+console.log(fruit);
+
+const myName = "Adam";
+console.log(myName);
+
+// myName = "Balthazar";
+// console.log(myName);
+
+const myName2 = "Balthazar";
+console.log(myName2);
+
+console.log('My name is '+ myName + ', not ' + myName2);
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+function addition() {
+    let a = 68;
+    let b = 1;
+    return a + b;
+}
+
+console.log(addition());
+
+let four = 4;
+console.log(four);
+console.log(typeof four);
+
+console.log(four + x);
+
+four = 'four';
+
+console.log(typeof four);
+
+const user = { name: 'Adam', location: 'Christchurch' };
+console.log(user);
+console.log(typeof user);
