ab9c9489ca74c5f3367151223fca02d2c73d4376
adam-sagar <adamschrissagar@gmail.com>
2023-02-22 18:53:49 +1300

    Lab 3.1

--- Module 3/Lab 3.1 - javascript/fundamentals.js
+++ Module 3/Lab 3.1 - javascript/fundamentals.js
@@ -1,15 +1,104 @@
-console.log(Number("" + 1 + 0))
-console.log(Number("" - 1 + 0))
-console.log(Number(true + false))
-console.log(Number(!true))
-console.log(Number(6 / "3"))
-console.log(Number("2" * "3"))
-console.log(Number(4 + 5 + "px"))
+console.log(typeof("" + 1 + 0));
+//10
+console.log(typeof("" - 1 + 0));
+
+console.log(Number(true + false));
+//1
+console.log(Number(!true));
+//0
+console.log(Number(6 / "3"));
+//2
+console.log(Number("2" * "3"));
+//6
+console.log(Number(4 + 5 + "px"));
+//Nan
+console.log(Number("$" + 4 + 5));
+//Nan
+console.log(Number("4" - 2));
+//2
+console.log(Number("4px" - 2));
+//Nan
+console.log(typeof(" -9 " + 5));
+
+console.log(Number(" -9 " - 5));
+//-14
+console.log(Number(null + 1));
+//1
+console.log(Number(undefined + 1));
+//Nan
+console.log(Number(undefined == null));
+
+console.log(Number(undefined === null));
+
+console.log(Number(" \t \n" -2));
+
+
+
+
 
 let three = "3"
 let four = "4"
 let thirty = "30"
 
-let addition = three + four
+let addition = three + four;
+// Not correct - "3" and "4" are concatenated with the + operator when strings
+console.log(addition);
 
-console.log(addition)
+let multiplication = three * four;
+// correct
+console.log(multiplication);
+
+let division = three / four;
+//correct
+console.log(division);
+
+let subtraction = three - four;
+//correct
+console.log(subtraction);
+
+let lessThan1 = three < four;
+//correct
+console.log(lessThan1);
+
+let lessThan2 = thirty < four;
+
+console.log(lessThan2);
+
+
+
+
+if (0) console.log('#1 zero is true')
+//won't print
+if ("0") console.log('#2 zero is true')
+//will print
+if (null) console.log('null is true')
+//won't print
+if (-1) console.log('negative is true')
+//will print
+if (1) console.log('positive is true')
+//will print
+
+
+
+
+
+// let a = 2, b = 3;
+// let result = `${a} + ${b} is `;
+// if (a + b < 10) {
+// result += 'less than 10';
+// } else {
+// result += 'greater than 10';
+// }
+
+let a = 2, b = 3;
+let result = `${a} + ${b} is ${a + b < 10 ? 'less than 10' : 'greater than 10'}`;
+
+console.log(result);
+
+
+
+
+
+function getGreeting(name) {
+return 'Hello ' + name + '!';
+}
--- Module 3/Lab 3.1 - javascript/practice.js
+++ Module 3/Lab 3.1 - javascript/practice.js
@@ -10,10 +10,7 @@ console.log(fruit);
 const myName = "Adam";
 console.log(myName);
 
-// myName = "Balthazar";
-// console.log(myName);
-
-const myName2 = "Balthazar";
+const myName2 = "Bob";
 console.log(myName2);
 
 console.log('My name is '+ myName + ', not ' + myName2);
@@ -43,3 +40,60 @@ console.log(typeof four);
 const user = { name: 'Adam', location: 'Christchurch' };
 console.log(user);
 console.log(typeof user);
+
+let height = 165
+
+if (height >= 150) {
+    height = 'is tall enough to ride';
+} else {
+    height = 'is not tall enough to ride';
+}
+
+console.log(height);
+
+//create a function getBMI(height, weight) which calculates and returns a person's BMI (google the formula)
+
+function getBMI (height, weight) {
+    let bmi = weight / (height * height);
+    return bmi
+}
+
+console.log(getBMI(1.83, 65));
+
+function convertTemp (celsius) {
+    let ctof = celsius * 1.8 + 32;
+    return ctof
+   
+}
+
+console.log(convertTemp(30));
+
+
+//create your own object with multiple properties
+//make sure some are multiple values
+//add some functions
+//write a function to print out all names/values using for ... in
+
+const book = {
+title: 'The Well of Ascension',
+author: 'Brandon Sanderson',
+published: 2007,
+genre: 'fantasy',
+characters: ['Vin', 'Elend', 'Dockson', 'Hammond'],
+reading() {
+    console.log("I'm reading " + this.title);
+}
+};
+
+console.log(book);
+book.reading();
+
+console.log(book.genre);
+
+console.log(book.characters[3]);
+
+// Try doing a for loop iteration here
+
+let mylastName = "sagar";
+
+console.log(`My last name is ${mylastName}`);
