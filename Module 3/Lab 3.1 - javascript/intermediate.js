55611330665647d0810a9047420c927d98b64d62
adam-sagar <adamschrissagar@gmail.com>
2023-02-24 22:57:12 +1300

    Updated resume-website

---
+++ Module 3/Lab 3.1 - javascript/intermediate.js
@@ -0,0 +1,10 @@
+console.log('\n QUESTION 1');
+
+let myString = 'the quick brown fox jumps over the lazy dog';
+
+function capitaliseFirstLetter(string) {
+    let newString = string.split();
+    return newString
+}
+
+console.log(capitaliseFirstLetter(myString))
