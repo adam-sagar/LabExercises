2d9981e531aec1c9fc277691d2144f46232d8c60
adam-sagar <adamschrissagar@gmail.com>
2023-02-25 15:40:23 +1300

    Fixed image resizing and added dropdown menu

--- Module 3/Lab 3.1 - javascript/intermediate.js
+++ Module 3/Lab 3.1 - javascript/intermediate.js
@@ -1,10 +1,22 @@
 console.log('\n QUESTION 1');
 
-let myString = 'the quick brown fox jumps over the lazy dog';
+//define function
+//accept one parameter of string to capitalise
+//split string to individual words using .split()
+//Loop through each word in list using for...of
+//use .toupperCase io capitalise first letter
+//concatenate capitalised letter back into word
+//add capitalised word to new list
+//join capitalised words back into single string
+//return capitalised string
 
-function capitaliseFirstLetter(string) {
-    let newString = string.split();
-    return newString
+function ucFirstLetters(string) {
+    let newString = string.split(" ");
+    for (let words of newString) {
+    words = words[0].toUpperCase() + words.slice(1);
+    }
+    return newString.join(" ");
 }
 
-console.log(capitaliseFirstLetter(myString))
+console.log(ucFirstLetters(myString));
+  
